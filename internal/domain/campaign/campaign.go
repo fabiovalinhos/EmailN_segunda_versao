f8ca8dc4cd67907dfb0bf39fe46270e3f33e4da5
fabiovalinhos <fabiovalinhos@gmail.com>
2026-05-18 10:56:39 -0300

    feat: adiciona 'constructor' NewCampaign no domain
    - Cria a função NewCampaign para inicialização da estrutura Campaign
    
    - Define valores iniciais e mapeamento de contatos

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -13,3 +13,18 @@ type Campaign struct {
 	Content   string
 	Contacts  []Contact
 }
+
+func NewCampaign(name, content string, emails []Contact) Campaign {
+	contacts := make([]Contact, len(emails))
+	for i, email := range emails {
+		contacts[i] = Contact{Email: email.Email}
+	}
+
+	return Campaign{
+		ID:        "1",
+		Name:      name,
+		CreatedOn: time.Now(),
+		Content:   content,
+		Contacts:  contacts,
+	}
+}
