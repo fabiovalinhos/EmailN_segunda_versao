176ba1c849035796b5c5a87dac0f5405b36ea62e
fabiovalinhos <fabiovalinhos@gmail.com>
2026-05-18 16:25:32 -0300

    feat: implementa geração automática de ID com xid
    - Adiciona biblioteca github.com/rs/xid
    
    - Atualiza NewCampaign para gerar ID único
    
    - Adiciona novos testes para validar ID e data de criação

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	name := "Test Campaign"
 	content := "This is a test campaign."
 	emails := []string{"email@example.com", "another@example.com"}
@@ -14,11 +15,12 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := NewCampaign(name, content, emails)
 
-	assert.Equal("1", campaign.ID, "Expected ID to be '1'")
 	assert.Equal(name, campaign.Name, "Expected Name to match")
 	assert.Equal(content, campaign.Content, "Expected Content to match")
 	assert.Len(campaign.Contacts, len(emails), "Expected number of contacts to match")
 
+	//// Sem usar o pacote testing/assert
+	////
 	// if campaign.ID != "1" {
 	// 	t.Errorf("Expected ID to be '1', got '%s'", campaign.ID)
 	// }
@@ -34,4 +36,27 @@ func TestNewCampaign(t *testing.T) {
 	// if len(campaign.Contacts) != len(emails) {
 	// 	t.Errorf("Expected %d contacts, got %d", len(emails), len(campaign.Contacts))
 	// }
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	name := "Test Campaign"
+	content := "This is a test campaign."
+	emails := []string{"email@example.com", "another@example.com"}
+	assert := assert.New(t)
+
+	campaign := NewCampaign(name, content, emails)
+
+	assert.NotNil(campaign.ID, "Expected ID to be not nil")
+}
+
+func Test_NewCampaign_CreatedOnIsNotNil(t *testing.T) {
+	name := "Test Campaign"
+	content := "This is a test campaign."
+	emails := []string{"email@example.com", "another@example.com"}
+	assert := assert.New(t)
+	now := time.Now().Add(-time.Minute) // Considera um tempo um pouco antes para evitar falhas devido a diferenças de tempo
+
+	campaign := NewCampaign(name, content, emails)
+
+	assert.Greater(campaign.CreatedOn, now)
 }
